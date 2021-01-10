42ada8689777e8fd04d148fb3d71c36eff32aa82
Rubens <rubao.ma@gmail.com>
2021-01-10 14:22:38 -0300

    add signup activity and your design

--- app/src/main/java/com/example/projemanag/SignInActivity.kt
+++ app/src/main/java/com/example/projemanag/SignInActivity.kt
@@ -2,32 +2,31 @@ package com.example.projemanag
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 import android.view.WindowManager
-import com.example.projemanag.databinding.ActivityIntroBinding
-import com.example.projemanag.databinding.ActivitySignUpBinding
+import com.example.projemanag.databinding.ActivitySignInBinding
 
-class SignUpActivity : AppCompatActivity() {
+class SignInActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySignUpBinding
+    private lateinit var binding: ActivitySignInBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
         window.setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
 
         setupActionBar()
-
     }
 
-
     private fun setupActionBar(){
-        setSupportActionBar(findViewById(R.id.toolbar_sign_up_activity))
+
+        setSupportActionBar(findViewById(R.id.toolbar_sign_in_activity))
 
         val actionBar = supportActionBar
         if (actionBar != null){
@@ -35,7 +34,7 @@ class SignUpActivity : AppCompatActivity() {
             actionBar.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_ios_24)
         }
 
-        binding.toolbarSignUpActivity.setNavigationOnClickListener {
+        binding.toolbarSignInActivity.setNavigationOnClickListener {
             onBackPressed()
         }
     }
