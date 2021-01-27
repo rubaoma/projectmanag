75e2755e760c9dd73805e2b1e9f5cc9c1353f07f
Rubens <rubao.ma@gmail.com>
2021-01-27 12:43:10 -0300

    add the parcelable in User Class

--- app/src/main/java/com/example/projemanag/model/User.kt
+++ app/src/main/java/com/example/projemanag/model/User.kt
@@ -1,5 +1,8 @@
 package com.example.projemanag.model
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class User(
 
     val id: String = "",
@@ -8,4 +11,36 @@ data class User(
     val image: String = "",
     val mobile: Long = 0,
     val fcmToken: String = "",
-)
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+            parcel.readString()!!,
+            parcel.readString()!!,
+            parcel.readString()!!,
+            parcel.readString()!!,
+            parcel.readLong(),
+            parcel.readString()!!) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) = with(parcel){
+        writeString(id)
+        writeString(name)
+        writeString(email)
+        writeString(image)
+        writeLong(mobile)
+        writeString(fcmToken)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<User> {
+        override fun createFromParcel(parcel: Parcel): User {
+            return User(parcel)
+        }
+
+        override fun newArray(size: Int): Array<User?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
